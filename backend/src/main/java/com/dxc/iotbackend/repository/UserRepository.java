65ffdb66830a9ce00df629b0919ab0f055482f91
Marc00002 <122208252+Marc00002@users.noreply.github.com>
2025-04-26 07:43:38 +0200

    Backend Merge and refactoring some functions

---
+++ backend/src/main/java/com/dxc/iotbackend/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.DXC.iotbackend.repository;
+
+
+
+
+import com.DXC.iotbackend.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    // Checks if an email already exists (used in registration)
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+
+    // Fetches the user by email (used in login)
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByUsername(String username);
+
+}

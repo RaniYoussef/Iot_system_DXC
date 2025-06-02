9d6a54731707a51f3b6d0fdba5e79dc35d523878
Marc00002 <122208252+Marc00002@users.noreply.github.com>
2025-06-02 19:15:30 +0200

    Restore routing

--- frontend/src/app/Dashboard/Choose-dashboard/Choose-dashboard.component.ts
+++ frontend/src/app/Dashboard/Choose-dashboard/Choose-dashboard.component.ts
@@ -17,4 +17,9 @@ export class ChooseDashboardComponent {
   goToTrafficDashboard(): void {
     this.router.navigate(['/traffic-dashboard']); 
   }
+    goToLightcDashboard(): void {
+    this.router.navigate(['/light-dashboard']); 
+  }
+
+  
 }
